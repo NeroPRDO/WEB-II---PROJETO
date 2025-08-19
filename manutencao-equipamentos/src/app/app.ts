21fdfc82212f3085e0598e3f26b27efec31f1bad
thiago cordeiro <thiagocordeiro2512@gmail.com>
2025-08-18 22:56:29 -0300

    feat: adicionando navbar ao projeto

--- manutencao-equipamentos/src/app/app.ts
+++ manutencao-equipamentos/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { botaoBasico } from './shared/Botoes/botaoBasico/botaoBasico';
+import { NavComponent } from './shared/Nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, botaoBasico],
+  imports: [RouterOutlet, botaoBasico, NavComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
