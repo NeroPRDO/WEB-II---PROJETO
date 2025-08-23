73f235aef571efebd1fc58147cf6b68d34fbaeda
thiago cordeiro <thiagocordeiro2512@gmail.com>
2025-08-23 17:24:52 -0300

    feat: Adicionando Rotas, e inicio do dashboard do usuario

---
+++ manutencao-equipamentos/src/app/dashboard/dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../shared/Nav/nav';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NavComponent],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css'
+})
+export class Dashboard {
+
+}
--- manutencao-equipamentos/src/app/shared/Nav/nav.ts
+++ manutencao-equipamentos/src/app/shared/Nav/nav.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrls: ['./nav.css']
 })
-export class NavComponent {}
+export class NavComponent { }
