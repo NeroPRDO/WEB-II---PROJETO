109644f49afbbfd6a95177bfd96bc99704163c75
Pedro <pedro.dallagnol.nr515@gmail.com>
2025-08-26 14:20:10 -0300

    adicionando CardOrcamento e interface.
    Adding func para orcamento

--- manutencao-equipamentos/src/app/dashboard/dashboard.ts
+++ manutencao-equipamentos/src/app/dashboard/dashboard.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { NavComponent } from '../shared/Nav/nav';
 import { CadastroAtendimento } from '../cadastro-atendimento/cadastro-atendimento';
 
+interface CardOrcamento {
+  id: string;
+  titulo: string;
+  estado: 'ORÇADA' | 'APROVADA' | 'REJEITADA' | 'ARRUMADA' | 'PAGA' | 'FINALIZADA' | 'ABERTA';
+}
+
+
 @Component({
   selector: 'app-dashboard',
-  imports: [NavComponent, CadastroAtendimento, RouterLink],
+  imports: [NavComponent, CadastroAtendimento, RouterLink, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
-
+  
+  orcamentos: CardOrcamento[] = [
+      { id: 'S-001', titulo: 'Notebook Lenovo', estado: 'ORÇADA' },
+      { id: 'S-002', titulo: 'Impressora HP',   estado: 'APROVADA' },
+      { id: 'S-003', titulo: 'PS4',             estado: 'REJEITADA' },
+  ];
+  
 }
