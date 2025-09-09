54a038451c00b99bbdde3ee77a55c72e27ce40ca
Matheus José <matheusjosclima@outlook.com>
2025-09-09 11:05:33 -0300

    Criação tela de pagamento

---
+++ manutencao-equipamentos/src/app/pagamento/pagamento.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../shared/Nav/nav';
+
+@Component({
+  selector: 'app-pagamento',
+  imports: [NavComponent],
+  templateUrl: './pagamento.html',
+  styleUrl: './pagamento.css'
+})
+export class Pagamento {
+
+}
