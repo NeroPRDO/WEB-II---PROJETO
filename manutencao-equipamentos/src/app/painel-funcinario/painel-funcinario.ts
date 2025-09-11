9aa6a38cba13a74e4f43eb05d36222605a082a9e
Maria Fernanda <maferzandona@gmail.com>
2025-09-11 19:32:13 -0300

    tabela com renderizacao e definicao flexiveis

--- manutencao-equipamentos/src/app/painel-funcinario/painel-funcinario.ts
+++ manutencao-equipamentos/src/app/painel-funcinario/painel-funcinario.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Panel } from '../shared/panel/panel';
 import { NavComponent } from '../shared/Nav/nav';
-import { TableChamado } from '../shared/table-chamado/table-chamado';
+import { TableChamado, TableColumn } from '../shared/table-chamado/table-chamado';
 import { BtnGradient } from '../shared/btn-gradient/btn-gradient';
+
 
 @Component({
   selector: 'app-painel-funcinario',
@@ -12,5 +13,49 @@ import { BtnGradient } from '../shared/btn-gradient/btn-gradient';
   styleUrl: './painel-funcinario.css'
 })
 export class PainelFuncinario {
+  data = [
+    {
+      "id": 1, 
+      "codigo": "123", 
+      "cliente": "maria fernanda", 
+      "descricao": "fdisjfdjs", 
+      "data_chamado": "05/07/2025"
+    },
+    {
+      "id": 2, 
+      "codigo": "456", 
+      "cliente": "thiago", 
+      "descricao": "gfdgd", 
+      "data_chamado": "05/07/2025"
+    },
+    {
+      "id": 3, 
+      "codigo": "789", 
+      "cliente": "pedro", 
+      "descricao": "bvcbAScv", 
+      "data_chamado": "05/07/2025"
+    },
+    {
+      "id": 4, 
+      "codigo": "456", 
+      "cliente": "dyego", 
+      "descricao": "fdsferw", 
+      "data_chamado": "05/07/2025"
+    },
+    {
+      "id": 3, 
+      "codigo": "789", 
+      "cliente": "matheus", 
+      "descricao": "hgfhfghfg", 
+      "data_chamado": "05/07/2025"
+    },
+    {
+      "id": 3, 
+      "codigo": "123", 
+      "cliente": "gabriela", 
+      "descricao": "gfjigfji", 
+      "data_chamado": "05/07/2007"
+    }
 
+  ]
 }
--- manutencao-equipamentos/src/app/shared/table-chamado/table-chamado.ts
+++ manutencao-equipamentos/src/app/shared/table-chamado/table-chamado.ts
@@ -1,59 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 
-interface Chamado {
-  id: number;
-  codigo: string;
-  estado: 'ORÇADA' | 'APROVADA' | 'REJEITADA' | 'ARRUMADA' | 'PAGA' | 'FINALIZADA' | 'ABERTA';
-  cliente?: string;
-  descricao?: string;
-  data?: string;
+export interface TableColumn {
+  id: string;
+  header: string;
+  render?: TemplateRef<any>; // You can type this more strictly with generics if needed
 }
 
 @Component({
   selector: 'app-table-chamado',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './table-chamado.html',
-  styleUrl: './table-chamado.css'
+  styleUrls: ['./table-chamado.css'],
 })
 export class TableChamado {
-
-  chamados: Chamado[] = [
-    {
-      id: 1, codigo: "S-001", estado: "ABERTA", 
-      cliente: "MARIA FERNANDA", descricao: "Asus Vivobook 15", 
-      data: "	02/07/2025"
-    },
-    {
-      id: 2, codigo: "S-002", estado: "ABERTA", 
-      cliente: "LUCAS B", descricao: "Lenovo Ideapad 3", 
-      data: "	07/05/2025"
-    },
-    {
-      id: 3, codigo: "S-003", estado: "ABERTA", 
-      cliente: "MATEUS Z", descricao: "	Macbook Air 13", 
-      data: "	07/02/2025"
-    },
-    {
-      id: 4, codigo: "S-004", estado: "ABERTA", 
-      cliente: "MARIA FERNANDA", descricao: "Asus Vivobook 15", 
-      data: "	02/07/2025"
-    },
-        {
-      id: 2, codigo: "S-002", estado: "ABERTA", 
-      cliente: "LUCAS B", descricao: "Lenovo Ideapad 3", 
-      data: "	07/05/2025"
-    },
-    {
-      id: 3, codigo: "S-003", estado: "ABERTA", 
-      cliente: "MATEUS Z", descricao: "	Macbook Air 13", 
-      data: "	07/02/2025"
-    },
-    {
-      id: 4, codigo: "S-004", estado: "ABERTA", 
-      cliente: "MARIA FERNANDA", descricao: "Asus Vivobook 15", 
-      data: "	02/07/2025"
-    }
-  ]
-  
+  @Input() colunas: TableColumn[] = [];
+  @Input() data: any[] = [];
 }
