257304913a66bc664806b281bbceb86c23aad1b5
thiago cordeiro <thiagocordeiro2512@gmail.com>
2025-09-20 17:45:38 -0300

    fix: corrigindo logica de botões conforme o estado

--- manutencao-equipamentos/src/app/dashboard/dashboard.ts
+++ manutencao-equipamentos/src/app/dashboard/dashboard.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { NavComponent } from '../shared/Nav/nav';
 import { TableChamado, TableColumn } from '../shared/table-chamado/table-chamado';
+import { SolicitacaoService } from '../services/solicitacao';
+
 
 export interface Solicitacao {
   id: number;
@@ -10,54 +12,24 @@ export interface Solicitacao {
   descricao: string;
   estado: 'ORÇADA' | 'APROVADA' | 'REJEITADA' | 'ARRUMADA' | string;
 }
-
 @Component({
   selector: 'app-dashboard',
+  standalone: true,
   imports: [NavComponent, RouterLink, CommonModule, TableChamado],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-
 export class Dashboard {
-
-  flag: string = 'Solicitacao';
-
-  isFlag(f: string) {
-    this.flag = f;
-  }
-
-  colunas: TableColumn[] = [
-    { id: 'dataHora', header: 'Data/Hora' },
-    { id: 'descricao', header: 'Descrição' },
-    { id: 'estado', header: 'Estado' },
-  ];
+  constructor(private solicitacaoService: SolicitacaoService) { }
 
   data = [
-    {
-      id: 1,
-      dataHora: '2025-09-15 10:00',
-      descricao: 'Notebook Dell Inspiron muito lento',
-      estado: 'ORÇADA',
-    },
-    {
-      id: 2,
-      dataHora: '2025-09-14 15:30',
-      descricao: 'Impressora HP não imprime',
-      estado: 'APROVADA',
-    },
-    {
-      id: 3,
-      dataHora: '2025-09-13 09:45',
-      descricao: 'PC Gamer não liga',
-      estado: 'REJEITADA',
-    },
-    {
-      id: 4,
-      dataHora: '2025-09-12 08:20',
-      descricao: 'MacBook Pro troca de tela',
-      estado: 'ARRUMADA',
-    },
+    { id: 1, dataHora: '2025-09-15 10:00', descricao: 'Notebook Dell Inspiron muito lento', estado: 'ORÇADA' },
+    { id: 2, dataHora: '2025-09-14 15:30', descricao: 'Impressora HP não imprime', estado: 'APROVADA' },
+    { id: 3, dataHora: '2025-09-13 09:45', descricao: 'PC Gamer não liga', estado: 'REJEITADA' },
+    { id: 4, dataHora: '2025-09-12 08:20', descricao: 'MacBook Pro troca de tela', estado: 'ARRUMADA' },
   ];
 
+  enviar(estado: string) {
+    this.solicitacaoService.solicitacao = estado;
+  }
 }
-
--- manutencao-equipamentos/src/app/visualizar-servico/visualizar-servico.ts
+++ manutencao-equipamentos/src/app/visualizar-servico/visualizar-servico.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../shared/Nav/nav';
-import { RouterModule } from '@angular/router';
-
+import { RouterLink, RouterModule } from '@angular/router';
+import { SolicitacaoService } from '../services/solicitacao';
 
 @Component({
   selector: 'app-visualizar-servico',
   standalone: true,
-  imports: [NavComponent,RouterModule],
+  imports: [NavComponent, RouterModule, RouterLink],
   templateUrl: './visualizar-servico.html',
   styleUrl: './visualizar-servico.css'
 })
 export class VisualizarServico {
+  solicitacao: string = '';
 
+  constructor(private solicitacaoService: SolicitacaoService) {
+    this.solicitacao = this.solicitacaoService.solicitacao;
+  }
 }
