e69766236282777d18fd37fcd78b1679c9fbd3cf
thiago cordeiro <thiagocordeiro2512@gmail.com>
2025-09-21 13:13:49 -0300

    feat: adicionando requisitos RF05 06 e 07

--- manutencao-equipamentos/src/app/visualizar-servico/visualizar-servico.ts
+++ manutencao-equipamentos/src/app/visualizar-servico/visualizar-servico.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../shared/Nav/nav';
 import { RouterLink, RouterModule } from '@angular/router';
 import { SolicitacaoService } from '../services/solicitacao';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-visualizar-servico',
   standalone: true,
-  imports: [NavComponent, RouterModule, RouterLink],
+  imports: [NavComponent, RouterModule, RouterLink, FormsModule],
   templateUrl: './visualizar-servico.html',
   styleUrl: './visualizar-servico.css'
 })
@@ -16,4 +17,30 @@ export class VisualizarServico {
   constructor(private solicitacaoService: SolicitacaoService) {
     this.solicitacao = this.solicitacaoService.solicitacao;
   }
+  visualizarOrcamento = false;
+  visualizarRejeitar = false;
+  motivoRejeicao = '';
+
+  visualizar() {
+    this.visualizarOrcamento = !this.visualizarOrcamento;
+  }
+
+  visuRejeitar() {
+    this.visualizarRejeitar = !this.visualizarRejeitar;
+  }
+  setMotivoRejeicao(s: string) {
+    this.motivoRejeicao = s;
+  }
+
+  confirmarRejeicao() {
+    if (!this.motivoRejeicao.trim()) {
+      window.alert('Por favor, informe o motivo da rejeição.');
+      return;
+    }
+    window.alert('Serviço Rejeitado\nMotivo: ' + this.motivoRejeicao);
+    this.visualizarRejeitar = false;
+    this.visualizarOrcamento = false;
+
+  }
+
 }
