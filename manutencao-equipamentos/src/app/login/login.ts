bc48c4f6eacc27a4efbc96e0aa0562b351fea88b
Dasko7b <dyegodasko14@gmail.com>
2025-10-07 09:46:50 -0300

    feat: adicionando verificação no formulario de login

--- manutencao-equipamentos/src/app/login/login.ts
+++ manutencao-equipamentos/src/app/login/login.ts
@@ -2,15 +2,31 @@ import { Component } from '@angular/core';
 import { BtnLogin } from '../shared/btn-login/btn-login';
 import { Panel } from '../shared/panel/panel';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [BtnLogin,Panel,RouterLink],
+  imports: [BtnLogin,Panel,RouterLink,CommonModule,FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 
 export class Login {
-  
+    login = {
+        email: '',
+        senha: ''
+    };
+
+    constructor() { }
+
+    logar(): void {
+        if (!this.login.email || !this.login.senha) {
+            console.error('Campos vazios!');
+            return;
+        }
+        console.log('Tentando logar com:', this.login);
+        // Aqui você chamaria seu serviço de autenticação
+    }
 }
 
