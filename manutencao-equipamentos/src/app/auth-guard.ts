142ba91a3feef073506cbe93ad48b6fb205fb371
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2025-11-11 19:37:39 -0300

    fix: placeholder authguard
    - removi o uso do loginService, que nao existe e por isso nao deixava o projeto rodar

--- manutencao-equipamentos/src/app/auth-guard.ts
+++ manutencao-equipamentos/src/app/auth-guard.ts
@@ -1,38 +1,38 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { LoginService } from './services/login-service';
+//import { LoginService } from './services/login-service';
 
 export const authGuard: CanActivateFn = (
-  route: ActivatedRouteSnapshot, 
+  route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
-  
-  const loginService = inject(LoginService);
+
+  //const loginService = inject(LoginService);
   const router = inject(Router);
-  
-  const usuarioLogado = loginService.usuarioLogado;
-  const url = state.url;
+  return true;
+  //  const usuarioLogado = loginService.usuarioLogado;
+  //  const url = state.url;
 
 
   //if (usuarioLogado) {
-    
-    //const requiredRole = route.data['role'];
-    
-    //if (!requiredRole) {
-      //return true;
-    //}
 
-   // if (usuarioLogado.role === requiredRole) {
-      //return true; 
-    //} else {
-      //console.warn(`Acesso negado. Usuário ${usuarioLogado.role} tentou acessar ${url} (requer ${requiredRole})`);
-      //if (usuarioLogado.role === 'FUNCIONARIO') {
-        //router.navigate(['/painel']); 
-      //} else {
-       // router.navigate(['/']); 
-      //}
-      //return false; 
-    //}
+  //const requiredRole = route.data['role'];
+
+  //if (!requiredRole) {
+  //return true;
+  //}
+
+  // if (usuarioLogado.role === requiredRole) {
+  //return true; 
+  //} else {
+  //console.warn(`Acesso negado. Usuário ${usuarioLogado.role} tentou acessar ${url} (requer ${requiredRole})`);
+  //if (usuarioLogado.role === 'FUNCIONARIO') {
+  //router.navigate(['/painel']); 
+  //} else {
+  // router.navigate(['/']); 
+  //}
+  //return false; 
+  //}
   //}
   //router.navigate(['/login'], { queryParams: { error: "Deve fazer o login para acessar " + url } });
   //return false; 
