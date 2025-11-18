eddbb2b2909fdc926e3ad561a231828f359e0745
Pedro <pedro.dallagnol.nr515@gmail.com>
2025-11-18 13:57:39 -0300

    Corrigindo erro em intermediario de relatorios
    Adicionando novamente os Arquivos.

---
+++ manutencao-equipamentos/src/app/pages/funcionario/relatorios-home/relatorios-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-relatorios-home',
+  imports: [RouterLink],
+  templateUrl: './relatorios-home.html',
+  styleUrls: ['./relatorios-home.css'],
+})
+export class RelatoriosHomeComponent {}
