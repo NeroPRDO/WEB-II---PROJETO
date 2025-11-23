1ceeb3e3bfda5d8edfcfc6aedb8a6c661e969ae2
thiago cordeiro <thiagocordeiro@bne.com.br>
2025-11-23 13:29:55 -0300

    feat: return id e role no login

--- backend-web-api/src/main/java/br/com/webdois/backend_web_api/controller/TokenController.java
+++ backend-web-api/src/main/java/br/com/webdois/backend_web_api/controller/TokenController.java
@@ -2,6 +2,7 @@ package br.com.webdois.backend_web_api.controller;
 
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.webdois.backend_web_api.dtos.LoginRequestDTO;
 import br.com.webdois.backend_web_api.dtos.LoginResponseDTO;
 import br.com.webdois.backend_web_api.dtos.RegisterRequestDTO;
+import br.com.webdois.backend_web_api.dtos.UserInfoDTO;
 import br.com.webdois.backend_web_api.entity.Usuario;
 import br.com.webdois.backend_web_api.repository.UsuarioRepository;
 import br.com.webdois.backend_web_api.service.MailService;
@@ -46,14 +48,15 @@ public class TokenController {
 
     @PostMapping("/Auth/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
-        var user = usuarioRepository.findByEmail(loginRequest.getEmail());
+       Optional<Usuario> user = usuarioRepository.findByEmail(loginRequest.getEmail());
+
 
         if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
             throw new BadCredentialsException("login inválido");
         }
 
         var now = java.time.Instant.now();
-        var expiresIn = 300L;
+        var expiresIn = 3600L;
 
         var scope = user.get().getRole().name();
 
@@ -66,7 +69,12 @@ public class TokenController {
                 .build();
 
         var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-        return ResponseEntity.ok(new LoginResponseDTO(jwtValue, expiresIn));
+
+        var userInfo = new UserInfoDTO(
+            user.get().getId(),
+            user.get().getRole().name()
+        );
+        return ResponseEntity.ok(new LoginResponseDTO(jwtValue, expiresIn, userInfo));
 
     }
 
---
+++ backend-web-api/src/main/java/br/com/webdois/backend_web_api/dtos/UserInfoDTO.java
@@ -0,0 +1,3 @@
+package br.com.webdois.backend_web_api.dtos;
+
+public record UserInfoDTO(Long id, String role) {}
