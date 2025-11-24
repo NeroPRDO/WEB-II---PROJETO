4b1ea7a9d86178b6358784887542ddcae52ab0d3
thiago cordeiro <thiagocordeiro2512@gmail.com>
2025-11-24 17:37:52 -0300

    fix: tela de pagamento

--- manutencao-equipamentos/src/app/pages/cliente/pagamento/pagamento.ts
+++ manutencao-equipamentos/src/app/pages/cliente/pagamento/pagamento.ts
@@ -2,44 +2,30 @@ import { Component, signal } from '@angular/core';
 import { NavComponent } from '../../../shared/Nav/nav';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { NgxMaskDirective } from "ngx-mask";
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pagamento',
   standalone: true, 
-  imports: [NavComponent, CommonModule, NgxMaskDirective, FormsModule],
+  imports: [NavComponent, CommonModule, FormsModule],
   templateUrl: './pagamento.html',
   styleUrl: './pagamento.css'
 })
 export class Pagamento {
-  // Variável para controlar a exibição da segunda etapa do formulário
-  isSecondStep: boolean = false;
-
-  // Variável para guardar a seleção
-  tipoPessoa = 'fisica';
-
-  // O construtor permite o Router para navegação
+  
   constructor(private router: Router) {}
 
-  // Função para avançar para a próxima etapa do form
   proximo(): void {
-    this.isSecondStep = true;
     alert(`Avança, após ter completado todos os campos`);
   }
 
-  // Função para voltar para a etapa anterior do form
   voltar(): void {
-    this.isSecondStep = false;
   }
-
-  // Função para "confirmar" o pagamento
   confirmarPagamento(): void {
     const agora = new Date();
     const horaFormatada = agora.toLocaleTimeString('pt-BR');
     const dataFormatada = agora.toLocaleDateString('pt-Br');
     
-    // O alert() funciona da mesma forma
     alert(`Pagamento Confirmado e Registrado ${dataFormatada} às ${horaFormatada}!`);
     this.router.navigate(['/../']);
   }
