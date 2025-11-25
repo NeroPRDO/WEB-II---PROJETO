83e945034b5938158158deeed3d63b6342b8fccb
Matheus José <matheusjosclima@outlook.com>
2025-11-24 22:55:46 -0300

    feat: Logout

--- manutencao-equipamentos/src/app/shared/Nav/nav.ts
+++ manutencao-equipamentos/src/app/shared/Nav/nav.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './nav.html',
   styleUrls: ['./nav.css']
 })
-export class NavComponent { }
+export class NavComponent {
+  private router = inject(Router);
+
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('auth_data');
+  }
+
+  logout(): void {
+    localStorage.removeItem('auth_data');
+    this.router.navigate(['/login']);
+  }
+}
